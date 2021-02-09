3f8ba5f44fa1a810c644952b02d2fb71625cc44d
blueperspective <pubcb84@free.fr>
2021-02-09 17:41:30 +0100

    Add project files.

---
+++ findmyzone/Program.cs
@@ -0,0 +1,52 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace findmyzone
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('c', "codepostal", Required = false, HelpText = "Liste des codes postaux")]
+            public IList<string> ZipCodes { get; set; }
+
+            [Option('v', "ville", Required = false, HelpText = "Liste des villes")]
+            public IList<string> Cities { get; set; }
+
+            [Option('c', "codepostal", Required = false, HelpText = "Liste des codes INSEE")]
+            public IList<string> InseeCode { get; set; }
+
+            [Option("pmin", Required = false, HelpText = "Surface minimale de parcelle (en m2)")]
+            public uint MinLotSurface { get; set; }
+
+            [Option("pmax", Required = false, HelpText = "Surface minimale de parcelle (en m2)")]
+            public uint MaxLotSurface { get; set; }
+
+            [Option("bmin", Required = false, HelpText = "Surface minimale de batiment (en m2)")]
+            public uint MinBuildingSurface { get; set; }
+
+            [Option("bmax", Required = false, HelpText = "Surface maximale de batiment (en m2)")]
+            public uint MaxBuildingSurface { get; set; }
+
+            [Option("Download",
+                    Default = true, HelpText = "Télécharger automatiquement les fichiers manquants")]
+            public bool Download { get; set; }
+        }
+
+        static async Task Main(string[] args)
+        {
+            var result = Parser.Default.ParseArguments<Options>(args);
+
+            await result.MapResult(async
+                x =>
+                {
+                    var downloader = new Downloader();
+                    await downloader.Download("35026");
+                },
+                errors => Task.FromResult(0)
+            );
+        }
+    }
+}
