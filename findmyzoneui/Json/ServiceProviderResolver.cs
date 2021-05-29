192606ebbf78290fc632e9d7cfa5a21cae03ac69
blueperspective <blueperspective>
2021-05-29 15:23:45 +0200

    using state

---
+++ findmyzoneui/Json/ServiceProviderResolver.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Serialization;
+using System;
+
+namespace findmyzoneui.Json
+{
+    class ServiceProviderResolver : DefaultContractResolver
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public ServiceProviderResolver(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        protected override JsonObjectContract CreateObjectContract(Type objectType)
+        {
+            if (serviceProvider.GetService(objectType) != null)
+            {
+                JsonObjectContract contract = base.CreateObjectContract(objectType);
+                contract.DefaultCreator = () => serviceProvider.GetService(objectType);
+                return contract;
+            }
+
+            return base.CreateObjectContract(objectType);
+        }
+    }
+}
