35e7b90768dab35ca35a78fd6d18ede2bf4bef66
blueperspective <blueperspective>
2021-09-26 16:42:21 +0200

    ui: max = min + 100 if max < min

--- findmyzoneui/Services/NewtonsoftJsonSuspensionDriver.cs
+++ findmyzoneui/Services/NewtonsoftJsonSuspensionDriver.cs
@@ -2,13 +2,9 @@
 using Newtonsoft.Json.Serialization;
 using ReactiveUI;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace findmyzoneui.Services
 {
