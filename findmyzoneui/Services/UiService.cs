c21f4f7db1d971aa431ee1a0229d37e5569ee358
blueperspective <blueperspective@lacazur.fr>
2026-02-21 21:08:32 +0100

    gen: update

---
+++ findmyzoneui/Services/UiService.cs
@@ -0,0 +1,80 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using MessageBox.Avalonia.DTO;
+using MessageBox.Avalonia.Enums;
+using findmyzoneui.Views;
+using System;
+using System.Threading.Tasks;
+
+namespace findmyzoneui.Services
+{
+    public class UiService : IUiService
+    {
+        private readonly Window parent;
+
+        public UiService(Window parent)
+        {
+            this.parent = parent;
+        }
+
+        public async Task<ButtonResult> Ask(string title, string message)
+        {
+            var msBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
+            {
+                ShowInCenter = true,
+                ButtonDefinitions = ButtonEnum.YesNo,
+                ContentTitle = title,
+                ContentMessage = message,
+                Icon = Icon.Plus
+            });
+
+            var res = await msBoxStandardWindow.Show();
+
+            return res;
+        }
+
+        public async Task ShowMessage(string title, string message)
+        {
+            var msBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                ContentTitle = title,
+                ContentMessage = message
+            });
+
+            await msBoxStandardWindow.Show();
+        }
+
+        public async Task ShowMessage(string title, string message, Icon icon, ButtonEnum buttonEnum)
+        {
+            var msBoxStandardWindow = MessageBox.Avalonia.MessageBoxManager.GetMessageBoxStandardWindow(new MessageBoxStandardParams
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                ContentTitle = title,
+                ContentMessage = message,
+                Icon = icon,
+                ButtonDefinitions = buttonEnum
+            });
+
+            await msBoxStandardWindow.Show();
+        }
+
+        public async Task ShowException(string title, string message, Exception e)
+        {
+            var desktop = Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+
+            if (desktop == null)
+            {
+                throw new NotImplementedException();
+            }
+
+            var window = new ExceptionDialog(title, message, e)
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            await window.ShowDialog(desktop.MainWindow);
+        }
+    }
+}
---
+++ findmyzoneui/Views/ExceptionDialog.axaml.cs
@@ -0,0 +1,51 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace findmyzoneui.Views
+{
+    public class ExceptionDialog : Window
+    {
+        public ExceptionDialog()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+        public ExceptionDialog(string title, string message, Exception e)
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+
+            var txtTitle = this.FindControl<TextBlock>("title");
+            txtTitle.Text = title;
+
+            var txtMessage = this.FindControl<TextBox>("message");
+            txtMessage.Text = message;
+
+            var txtStackTrace = this.FindControl<TextBox>("stacktrace");
+            txtStackTrace.Text = e.ToString();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        public void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            var txtMessage = this.FindControl<TextBox>("stacktrace");
+            Application.Current.Clipboard.SetTextAsync(txtMessage.Text);
+        }
+    }
+}
