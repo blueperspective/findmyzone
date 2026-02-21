feacb40a0b9597881038dd2cc32569486d2f7be4
blueperspective <blueperspective@lacazur.fr>
2026-02-21 21:08:32 +0100

    ui: result count

--- findmyzone/Geo/ZoneFinderResult.cs
+++ findmyzone/Geo/ZoneFinderResult.cs
@@ -15,25 +15,32 @@ namespace findmyzone.Geo
 
         public override string ToString()
         {
-            var builder = new StringBuilder();
-            builder.AppendLine($"\nParcelle {Feature.Attributes["id"]}, " +
-                    $"prefixe: {Feature.Attributes["prefixe"]}, " +
-                    $"section: {Feature.Attributes["section"]}, " +
-                    $"numero: {Feature.Attributes["numero"]}");
-            builder.AppendLine($"  position géographique: {Feature.Geometry.Centroid.Y} {Feature.Geometry.Centroid.X}");
-            builder.AppendLine($"  contenance: {Feature.Attributes["contenance"]} m2");
-            builder.AppendLine($"  surface calculée: {Math.Floor(ProjZoneGeometry.Area)} m2");
-            builder.AppendLine($"  {ProjBuildingGeometries.Count} batiments, surface. calculée: {Math.Floor(ProjBuildingGeometries.Sum(x => x.Area))} m2");
-
-            if (ProjBuildingGeometries.Any())
+            try
             {
-                foreach (var building in ProjBuildingGeometries)
-                {
-                    builder.AppendLine($"    - {Math.Floor(building.Area)} m2");
-                }
-            }
+                var builder = new StringBuilder();
+                builder.AppendLine($"\nParcelle {Feature?.Attributes["id"]}, " +
+                        $"prefixe: {Feature?.Attributes["prefixe"]}, " +
+                        $"section: {Feature?.Attributes["section"]}, " +
+                        $"numero: {Feature?.Attributes["numero"]}");
+                builder.AppendLine($"  position géographique: {Feature?.Geometry.Centroid.Y} {Feature?.Geometry.Centroid.X}");
+                builder.AppendLine($"  contenance: {Feature?.Attributes["contenance"]} m2");
+                builder.AppendLine($"  surface calculée: {Math.Floor(ProjZoneGeometry.Area)} m2");
+                builder.AppendLine($"  {ProjBuildingGeometries.Count} batiments, surface. calculée: {Math.Floor(ProjBuildingGeometries.Sum(x => x.Area))} m2");
 
-            return builder.ToString();
+                if (ProjBuildingGeometries.Any())
+                {
+                    foreach (var building in ProjBuildingGeometries)
+                    {
+                        builder.AppendLine($"    - {Math.Floor(building.Area)} m2");
+                    }
+                }
+
+                return builder.ToString();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
     }
 }
